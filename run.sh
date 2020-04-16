0c283b8ba94ca664c607df5e3977cd8c7a37fe18
Mariana Bortnichuk <mariana.bortnichuk@gmail.com>
2020-04-16 19:45:47 +0300

    adding run.sh file to simplify prog demo

---
+++ run.sh
@@ -0,0 +1,3 @@
+java -jar fixmerouter/target/fixmerouter-1.0-SNAPSHOT.jar
+java -jar fixmemarket/target/fixmemarket-1.0-SNAPSHOT.jar
+java -jar fixmebroker/target/fixmebroker-1.0-SNAPSHOT.jar 500000 buy gold 1 300
